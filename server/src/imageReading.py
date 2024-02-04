bf92f9cf4db7cdbd4f3563b60e882e980b867798
Field4 <lauren.eb.field@gmail.com>
2024-02-04 00:43:27 +0000

    Should work on most, a bit of clean up needed on it but will do that later

--- server/src/imageReading.py
+++ server/src/imageReading.py
@@ -9,7 +9,7 @@ def extract_text_from_image(image_path):
     result = reader.readtext(image_path)
 
     # Extract and return the text
-    extracted_text = ' '.join([entry[1] for entry in result])
+    extracted_text = ';'.join([entry[1] for entry in result])
 
     # Remove text from the image
     image = cv2.imread(image_path)
@@ -21,17 +21,15 @@ def extract_text_from_image(image_path):
         x_min, y_min = map(int, [bbox[0][0], bbox[0][1]])
         x_max, y_max = map(int, [bbox[2][0], bbox[2][1]])
 
-        # Fill the bounding box with white color to remove text
-        cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 255, 255), -1)
 
     # Save the image with text removed
-    output_image_path = "if_test.png"
+    output_image_path = "if_test4.png"
     cv2.imwrite(output_image_path, image)
 
     return extracted_text, output_image_path
 
 # Example usage:
-input_image_path = "if_test.png"
+input_image_path = "if_test4.png"
 extracted_text, output_image_path = extract_text_from_image(input_image_path)
 print("Extracted Text:", extracted_text)
 print("Output Image with Text Removed:", output_image_path)
